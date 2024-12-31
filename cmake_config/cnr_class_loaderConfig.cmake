c6468d28b0304f903e5b424e80d23e727ccbbeae
Cesare Tonola <c.tonola001@unibs.it>
2024-12-31 11:53:34 +0100

    cmake.in

--- cmake_config/cnr_class_loaderConfig.cmake
+++
@@ -1,8 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-
-
-find_dependency(Boost REQUIRED COMPONENTS thread system filesystem)
-find_dependency(Poco REQUIRED COMPONENTS Foundation)
-
-include("${CMAKE_CURRENT_LIST_DIR}/cnr_class_loaderTargets.cmake")
